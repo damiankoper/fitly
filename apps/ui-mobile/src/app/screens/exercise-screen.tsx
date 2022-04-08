0a3f0e4fe99b8a468d185ee6485ad59de4072d41
dkrzysztof <krzysiuu75@gmail.com>
2022-04-08 17:36:38 +0200

    fix: fixed error - all objects from map should have key

--- apps/ui-mobile/src/app/screens/exercise-screen.tsx
+++ apps/ui-mobile/src/app/screens/exercise-screen.tsx
@@ -4,28 +4,28 @@ import { EXERCISES_ENUM } from '../config';
 import { StyleSheet, View } from 'react-native';
 import { ActivityCardSmall } from '../components/cards/activity-card-small';
 export const ExerciseScreen = () => {
-  const exercises = Object.values(EXERCISES_ENUM);
+	const exercises = Object.values(EXERCISES_ENUM);
 
-  return (
-    <Layout>
-      <Text category="h4" style={styles.title}>
-        Choose activity
-      </Text>
-      {exercises.map((ex) => (
-        <View style={styles.cardWrapper}>
-          <ActivityCardSmall activity={ex} key={ex} />
-        </View>
-      ))}
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Text category="h4" style={styles.title}>
+				Choose activity
+			</Text>
+			{exercises.map((ex, i) => (
+				<View style={styles.cardWrapper} key={i}>
+					<ActivityCardSmall activity={ex} key={ex} />
+				</View>
+			))}
+		</Layout>
+	);
 };
 
 const styles = StyleSheet.create({
-  title: {
-    textAlign: 'center',
-    marginBottom: 16,
-  },
-  cardWrapper: {
-    marginBottom: 8,
-  },
+	title: {
+		textAlign: 'center',
+		marginBottom: 16,
+	},
+	cardWrapper: {
+		marginBottom: 8,
+	},
 });
