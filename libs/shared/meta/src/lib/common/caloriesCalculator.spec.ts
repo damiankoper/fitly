e28287774dd0932a92743da82ed14f6cde4bf1dd
damiankoper <kopernickk@gmail.com>
2022-05-12 18:43:52 +0200

    chore: format

--- libs/shared/meta/src/lib/common/caloriesCalculator.spec.ts
+++ libs/shared/meta/src/lib/common/caloriesCalculator.spec.ts
@@ -1,24 +1,26 @@
-import { calculateCalories } from './caloriesCalculator'
-import { ActivityType } from '../enums/activityType.enum'
+import { calculateCalories } from './caloriesCalculator';
+import { ActivityType } from '../enums/activityType.enum';
 
-describe("Calculate calories for 70kg and 30min", ()=>{
-    it("SITUPS should equals 294", ()=>{
-        expect(calculateCalories(ActivityType.SITUPS, 70, 30)).toBe(294)
-    })
+describe('Calculate calories for 70kg and 30min', () => {
+  it('SITUPS should equals 294', () => {
+    expect(calculateCalories(ActivityType.SITUPS, 70, 30)).toBe(294);
+  });
 
-    it("SQUATS should equals 275.625", ()=>{
-        expect(calculateCalories(ActivityType.SQUATS, 70, 30)).toBe(275.625)
-    })
-    
-    it("PUSHUPS should equals 312.375", ()=>{
-        expect(calculateCalories(ActivityType.PUSHUPS, 70, 30)).toBe(312.375)
-    })
-    
-    it("STAR_JUMPS should equals 183.75", ()=>{
-        expect(calculateCalories(ActivityType.STAR_JUMPS, 70, 30)).toBe(183.75)
-    })
-    
-    it("UNKNOWN should equals 0", ()=>{
-        expect(calculateCalories(ActivityType.UNKNOWN, 69, 420)).toBe(0)
-    })
-})
+  it('SQUATS should equals 275.625', () => {
+    expect(calculateCalories(ActivityType.SQUATS, 70, 30)).toBe(275.625);
+  });
+
+  it('PUSHUPS should equals 312.375', () => {
+    expect(calculateCalories(ActivityType.PUSHUPS, 70, 30)).toBe(312.375);
+  });
+
+  it('STAR_JUMPS should equals 183.75', () => {
+    expect(calculateCalories(ActivityType.STAR_JUMPS, 70, 30)).toBe(183.75);
+  });
+
+  it('UNKNOWN should throw exception', () => {
+    expect(() => {
+      calculateCalories(ActivityType.UNKNOWN, 69, 420);
+    }).toThrow();
+  });
+});
--- libs/shared/meta/src/lib/common/caloriesCalculator.ts
+++ libs/shared/meta/src/lib/common/caloriesCalculator.ts
@@ -1,30 +1,30 @@
-import { ActivityType } from '../enums/activityType.enum'
+import { ActivityType } from '../enums/activityType.enum';
 
-export function calculateCalories(activityType : ActivityType, weight : number, duration : number) : number {
+export function calculateCalories(
+  activityType: ActivityType,
+  weight: number,
+  duration: number
+): number {
+  // Metabolic equivalent for task
+  let met = 0;
 
-    // Metabolic equivalent for task
-    let met = 0;
+  switch (activityType) {
+    case ActivityType.SITUPS:
+      met = 8;
+      break;
+    case ActivityType.SQUATS:
+      met = 7.5;
+      break;
+    case ActivityType.PUSHUPS:
+      met = 8.5;
+      break;
+    case ActivityType.STAR_JUMPS:
+      met = 5;
+      break;
+    default:
+      throw new Error('Invalid activityType');
+  }
 
-    switch(activityType)
-    {
-        case ActivityType.SITUPS:
-            met = 8;
-            break;
-        case ActivityType.SQUATS:
-            met = 7.5;
-            break;
-        case ActivityType.PUSHUPS:
-            met = 8.5;
-            break;
-        case ActivityType.STAR_JUMPS:
-            met = 5;
-            break;
-        default:
-            met = 0;
-            break;
-    }
-
-    let calories = duration * (met * 3.5 * weight) / 200;
-
-    return calories;
+  const calories = (duration * (met * 3.5 * weight)) / 200;
+  return calories;
 }
