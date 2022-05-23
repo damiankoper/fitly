50dc66e71360e47a5de697b6a494f05dcc5dcecc
dkrzysztof <krzysiuu75@gmail.com>
2022-05-23 13:14:09 +0200

    stage: Stage after merge

--- apps/ui-mobile/src/app/native-modules/screens/app-init-screen.tsx
+++ apps/ui-mobile/src/app/native-modules/screens/app-init-screen.tsx
@@ -5,97 +5,95 @@ import { useDispatch, useSelector } from 'react-redux';
 import LoadingScreen from '../../components/loading-screen/LoadingScreen';
 import BluetoothModule from '../BluetoothModule';
 import {
-	setConnectedDevice,
-	restoreConnectionStop,
-	restoreConnectionStart,
-	clearConnectedDevice,
+  setConnectedDevice,
+  restoreConnectionStop,
+  restoreConnectionStart,
+  clearConnectedDevice,
 } from '../../state/app/app.slice';
 import { RootState } from '../../state/root.reducer';
 
 const CONNECTION_CHECKING_INTERVAL_SECONDS = 2000;
 
-export interface AppInitScreenProps {
-	metawear: MetaWear;
+export interface AppInitScreenProps extends React.PropsWithChildren<{}> {
+  metawear: MetaWear;
 }
 
 const AppInitScreen: React.FC<AppInitScreenProps> = ({
-	metawear,
-	children,
+  metawear,
+  children,
 }) => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const isRestoringConnection = useSelector(
-		(state: RootState) => state.app.status.isRestoringConnection
-	);
+  const isRestoringConnection = useSelector(
+    (state: RootState) => state.app.status.isRestoringConnection
+  );
 
-	async function restoreConnection() {
-		const lastConnectedMac =
-			await BluetoothModule.getSavedConnectedBluetoothDevice();
-		if (lastConnectedMac == null) {
-			showNotification('No metawear device saved');
-		} else {
-			try {
-				await metawear.connect(lastConnectedMac);
-				dispatch(
-					setConnectedDevice({
-						deviceName: 'MetaWear',
-						deviceAddress: lastConnectedMac,
-					})
-				);
-				dispatch(restoreConnectionStop());
-				showNotification('Connection with MetaWear device restored!');
-				startConnectionValidationTimeout();
-			} catch (error) {
-				dispatch(restoreConnectionStop());
-				dispatch(clearConnectedDevice());
-				showNotification(
-					'Connection with MetaWear could not be restored!'
-				);
-			}
-		}
-	}
+  async function restoreConnection() {
+    const lastConnectedMac =
+      await BluetoothModule.getSavedConnectedBluetoothDevice();
+    if (lastConnectedMac == null) {
+      showNotification('No metawear device saved');
+    } else {
+      try {
+        await metawear.connect(lastConnectedMac);
+        dispatch(
+          setConnectedDevice({
+            deviceName: 'MetaWear',
+            deviceAddress: lastConnectedMac,
+          })
+        );
+        dispatch(restoreConnectionStop());
+        showNotification('Connection with MetaWear device restored!');
+        startConnectionValidationTimeout();
+      } catch (error) {
+        dispatch(restoreConnectionStop());
+        dispatch(clearConnectedDevice());
+        showNotification('Connection with MetaWear could not be restored!');
+      }
+    }
+  }
 
-	function startConnectionValidationTimeout() {
-		setTimeout(() => {
-			checkConnection();
-		}, CONNECTION_CHECKING_INTERVAL_SECONDS);
-	}
+  function startConnectionValidationTimeout() {
+    setTimeout(() => {
+      checkConnection();
+    }, CONNECTION_CHECKING_INTERVAL_SECONDS);
+  }
 
-	async function checkConnection() {
-		const isConnected =
-			await BluetoothModule.checkConnectionWithMetaWearDevice();
-		if (!isConnected) {
-			dispatch(restoreConnectionStart());
-			await restoreConnection();
-		} else {
-			startConnectionValidationTimeout();
-		}
-	}
+  async function checkConnection() {
+    const isConnected =
+      await BluetoothModule.checkConnectionWithMetaWearDevice();
+    if (!isConnected) {
+      dispatch(restoreConnectionStart());
+      await restoreConnection();
+    } else {
+      startConnectionValidationTimeout();
+    }
+  }
 
-	useEffect(() => {
-		restoreConnection();
-		return () => {
-			metawear.disconnect();
-		};
-	}, []);
+  useEffect(() => {
+    restoreConnection();
+    return () => {
+      metawear.disconnect();
+    };
+  }, []);
 
-	const handleCloseLoadingScreen = () => {
-		dispatch(restoreConnectionStop());
-		dispatch(clearConnectedDevice());
-	};
+  const handleCloseLoadingScreen = () => {
+    dispatch(restoreConnectionStop());
+    dispatch(clearConnectedDevice());
+  };
 
-	if (isRestoringConnection) {
-		return (
-			<LoadingScreen
-				subText="Restoring connection with device. Please wait."
-				additionalButton
-				buttonText="Stop searching"
-				onButtonPress={handleCloseLoadingScreen}
-			/>
-		);
-	}
+  if (isRestoringConnection) {
+    return (
+      <LoadingScreen
+        subText="Restoring connection with device. Please wait."
+        additionalButton
+        buttonText="Stop searching"
+        onButtonPress={handleCloseLoadingScreen}
+      />
+    );
+  }
 
-	return children;
+  return <>{children}</>;
 };
 
 export default AppInitScreen;
--- apps/ui-mobile/src/app/native-modules/screens/home-screen.tsx
+++ apps/ui-mobile/src/app/native-modules/screens/home-screen.tsx
@@ -13,168 +13,158 @@ import ActivityLineChart from '../../components/charts/ActivityLineChart';
 import DropShadowWrapper from '../../components/gradients/drop-shadow';
 
 export const StepsIcon = () => {
-	const theme = useTheme();
+  const theme = useTheme();
 
-	return (
-		<Icon
-			name="pin-outline"
-			style={styles.icon}
-			fill={theme['color-primary-default']}
-		/>
-	);
+  return (
+    <Icon
+      name="pin-outline"
+      style={styles.icon}
+      fill={theme['color-primary-default']}
+    />
+  );
 };
 
 export const CaloriesIcon = () => {
-	const theme = useTheme();
+  const theme = useTheme();
 
-	return (
-		<Icon
-			name="flash-outline"
-			style={styles.icon}
-			fill={theme['color-danger-default']}
-		/>
-	);
+  return (
+    <Icon
+      name="flash-outline"
+      style={styles.icon}
+      fill={theme['color-danger-default']}
+    />
+  );
 };
 
 export const TimeIcon = () => {
-	const theme = useTheme();
+  const theme = useTheme();
 
-	return (
-		<Icon
-			name="clock-outline"
-			style={styles.icon}
-			fill={theme['color-basic-700']}
-		/>
-	);
+  return (
+    <Icon
+      name="clock-outline"
+      style={styles.icon}
+      fill={theme['color-basic-700']}
+    />
+  );
 };
 
 export const HomeScreen: React.FC<{}> = () => {
-	return (
-		<Layout>
-			<ScrollView contentContainerStyle={styles.defaultPadding}>
-				<UserCard name="Jan Nikodem" title="Master of squats" />
-				<ActivityLineChart />
-				<View style={[styles.cardRow, styles.overflowVisible]}>
-					<View
-						style={[
-							styles.cardColumn,
-							styles.leftColumn,
-							styles.overflowVisible,
-						]}
-					>
-						<DataCardLarge
-							Icon={StepsIcon}
-							name="Steps"
-							quantity={2137}
-							theme="primary"
-						/>
-						<DataCardLarge
-							Icon={CaloriesIcon}
-							name="Calories"
-							quantity={1690}
-							theme="danger"
-						/>
-					</View>
-					<View style={[styles.cardColumn, styles.rightColumn]}>
-						<View style={styles.smallCardRow}>
-							<BluetoothStatus
-								touchable
-								touchableStyles={styles.bluetoothWrapper}
-								renderOverlay
-								renderSubText
-								iconSize="large"
-							/>
-							<View style={styles.separator} />
-							<DataCardSmall
-								data="45"
-								activity="Running"
-								style={styles.activitiesWrapper}
-							/>
-						</View>
-						<DataCardLarge
-							Icon={TimeIcon}
-							name="Time spent"
-							quantity={3723}
-							theme="basic"
-						/>
-						<View style={styles.separator} />
-						<DataCardSmall data="45" activity="running" />
-					</View>
-				</View>
+  return (
+    <Layout>
+      <ScrollView contentContainerStyle={styles.defaultPadding}>
+        <UserCard name="Jan Nikodem" title="Master of squats" />
+        <ActivityLineChart />
+        <View style={[styles.cardRow, styles.overflowVisible]}>
+          <View
+            style={[
+              styles.cardColumn,
+              styles.leftColumn,
+              styles.overflowVisible,
+            ]}
+          >
+            <DataCardLarge
+              Icon={StepsIcon}
+              name="Steps"
+              quantity={2137}
+              theme="primary"
+            />
+            <DataCardLarge
+              Icon={CaloriesIcon}
+              name="Calories"
+              quantity={1690}
+              theme="danger"
+            />
+          </View>
+          <View style={[styles.cardColumn, styles.rightColumn]}>
+            <View style={styles.smallCardRow}>
+              <BluetoothStatus
+                touchable
+                touchableStyles={styles.bluetoothWrapper}
+                renderOverlay
+                renderSubText
+                iconSize="large"
+              />
+              <View style={styles.separator} />
+              <DataCardSmall
+                data="45"
+                activity="Running"
+                style={styles.activitiesWrapper}
+              />
+            </View>
+            <DataCardLarge
+              Icon={TimeIcon}
+              name="Time spent"
+              quantity={3723}
+              theme="basic"
+            />
+            <View style={styles.separator} />
+            <DataCardSmall data="45" activity="running" />
+          </View>
+        </View>
 
-				<View style={styles.bottomCard}>
-					<ActivityCardLarge
-						theme={'primary'}
-						activity={ActivityType.SQUATS}
-						date="Yesterday, 8 Mar"
-						kcal={231}
-						time="2:32"
-						count={31}
-					/>
-				</View>
-			</View>
-
-			<View style={styles.bottomCard}>
-				<ActivityCardLarge
-					activity={ActivityType.SQUATS}
-					date="Yesterday, 8 Mar"
-					kcal={231}
-					time="2:32"
-					count={31}
-				/>
-			</View>
-		</>
-	);
+        <View style={styles.bottomCard}>
+          <ActivityCardLarge
+            theme={'primary'}
+            activity={ActivityType.SQUATS}
+            date="Yesterday, 8 Mar"
+            kcal={231}
+            time="2:32"
+            count={31}
+          />
+        </View>
+      </ScrollView>
+    </Layout>
+  );
 };
 
 const styles = StyleSheet.create({
-	defaultPadding: {
-		padding: 16,
-		overflow: 'visible',
-	},
-	placeholder: {
-		marginVertical: 16,
-		height: 170,
-	},
-	cardColumn: {
-		flex: 1,
-		height: 170,
-		flexDirection: 'column',
-		justifyContent: 'space-between',
-	},
-	bluetoothWrapper: {
-		flex: 1,
-		width: '50%',
-		padding: 0,
-	},
-	activitiesWrapper: {
-		width: '50%',
-	},
-	leftColumn: {
-		marginRight: 4,
-	},
-	rightColumn: {
-		marginLeft: 4,
-	},
-	cardRow: {
-		flexDirection: 'row',
-		flex: 1,
-		height: 170,
-	},
-	icon: {
-		height: 40,
-		width: 40,
-	},
-	smallCardRow: {
-		flexDirection: 'row',
-	},
-	separator: {
-		width: 8,
-	},
-	bottomCard: {
-		marginVertical: 21,
-	},
-	overflowVisible: {
-		overflow: 'visible',
-	},
+  defaultPadding: {
+    padding: 16,
+    overflow: 'visible',
+  },
+  placeholder: {
+    marginVertical: 16,
+    height: 170,
+  },
+  cardColumn: {
+    flex: 1,
+    height: 170,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  bluetoothWrapper: {
+    flex: 1,
+    width: '50%',
+    padding: 0,
+  },
+  activitiesWrapper: {
+    width: '50%',
+  },
+  leftColumn: {
+    marginRight: 4,
+  },
+  rightColumn: {
+    marginLeft: 4,
+  },
+  cardRow: {
+    flexDirection: 'row',
+    flex: 1,
+    height: 170,
+  },
+  icon: {
+    height: 40,
+    width: 40,
+  },
+  smallCardRow: {
+    flexDirection: 'row',
+  },
+  separator: {
+    width: 8,
+  },
+  bottomCard: {
+    marginVertical: 21,
+  },
+  overflowVisible: {
+    overflow: 'visible',
+  },
 });
