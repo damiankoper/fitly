4cdb143b77baaf5c4c128761d3425c36cfba3b0f
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2022-05-28 12:27:38 +0200

    feat: add top bar navigation

--- apps/ui-mobile/src/app/components/navigation/top-nav.tsx
+++ apps/ui-mobile/src/app/components/navigation/top-nav.tsx
@@ -1,43 +1,46 @@
 import React from 'react';
 import {
-	Icon,
-	Layout,
-	TopNavigation,
-	TopNavigationAction,
+  Icon,
+  Layout,
+  TopNavigation,
+  TopNavigationAction,
 } from '@ui-kitten/components';
-import { ImageProps, StyleSheet } from 'react-native';
+import { ImageProps, StyleSheet, TouchableOpacity } from 'react-native';
 import { RenderProp } from '@ui-kitten/components/devsupport';
 import { BluetoothStatus } from '../icons/bluetooth-status';
+import * as RootNavigation from '../navigation/root-navigation';
 
 type IconProp = RenderProp<Partial<ImageProps>>;
 const PersonIcon: IconProp = (props) => (
-	<Icon {...props} name="person-outline" />
+  <TouchableOpacity onPress={() => RootNavigation.navigate('Settings')}>
+    <Icon {...props} name="person-outline" />
+  </TouchableOpacity>
 );
 
 export const TopNav = () => {
-	const renderRightActions = () => <TopNavigationAction icon={PersonIcon} />;
-	const renderLeftActions = () => (
-		<TopNavigationAction
-			icon={() => <BluetoothStatus touchable iconSize="small" />}
-		/>
-	);
+  const renderRightActions = () => <TopNavigationAction icon={PersonIcon} />;
+  const renderLeftActions = () => (
+    <TopNavigationAction
+      icon={() => <BluetoothStatus touchable iconSize="small" />}
+    />
+  );
 
-	return (
-		<Layout>
-			<TopNavigation
-				alignment="center"
-				title="Fitly"
-				style={styles.topnav}
-				accessoryLeft={renderLeftActions}
-				accessoryRight={renderRightActions}
-			/>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <TopNavigation
+        alignment="center"
+        title="Fitly"
+        style={styles.topnav}
+        accessoryLeft={renderLeftActions}
+        accessoryRight={renderRightActions}
+      />
+    </Layout>
+  );
 };
 
 const styles = StyleSheet.create({
-	topnav: {
-		borderBottomColor: 'lightgray',
-		borderBottomWidth: 1,
-	},
+  topnav: {
+    borderBottomColor: 'lightgray',
+    borderBottomWidth: 1,
+  },
 });
