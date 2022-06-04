af879e42f9d8b087ecb3c04eb9a27606443d5909
damiankoper <kopernickk@gmail.com>
2022-06-04 11:40:50 +0200

    fix: uuid in default data

--- apps/ui-mobile/src/app/common/utils.ts
+++ apps/ui-mobile/src/app/common/utils.ts
@@ -6,7 +6,7 @@ import {
 } from '@fitly/shared/meta';
 import { Sex } from 'libs/shared/meta/src/lib/enums/sex.enum';
 import { DateTime, Interval } from 'luxon';
-
+import { v4 as uuidv4 } from 'uuid';
 export function formatActivityString(activity: string) {
   // changes enum names to better looking
   return activity[0].toUpperCase() + activity.slice(1).replace('_', ' ');
@@ -20,7 +20,8 @@ export const DEFAULT_USER: User = {
   height: 180,
   sex: Sex.MALE,
 };
-
+const uuid1 = uuidv4();
+const uuid2 = uuidv4();
 export const DEFAULT_ACTIVITY_SESSIONS: ActivitySession[] = [
   {
     id: 2,
@@ -30,6 +31,7 @@ export const DEFAULT_ACTIVITY_SESSIONS: ActivitySession[] = [
     ),
     activities: [
       new ActivityTrackingMeta(
+        uuid1,
         Interval.fromDateTimes(
           DateTime.fromSQL('2022-05-22 08:15:00'),
           DateTime.fromSQL('2022-05-22 08:17:00')
@@ -47,6 +49,7 @@ export const DEFAULT_ACTIVITY_SESSIONS: ActivitySession[] = [
     ),
     activities: [
       new ActivityTrackingMeta(
+        uuid2,
         Interval.fromDateTimes(
           DateTime.fromSQL('2022-05-26 08:15:00'),
           DateTime.fromSQL('2022-05-26 08:15:15')
@@ -55,6 +58,7 @@ export const DEFAULT_ACTIVITY_SESSIONS: ActivitySession[] = [
         2
       ),
       new ActivityTrackingMeta(
+        uuid2,
         Interval.fromDateTimes(
           DateTime.fromSQL('2022-05-26 08:15:00'),
           DateTime.fromSQL('2022-05-26 08:15:15')
